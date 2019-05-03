067b0c67048157b8c6955bac7b18142862d52c24
April King <april@mozilla.com>
2019-05-03 11:26:33 -0500

    Fix 0.9.8h < 0.9.8 and four digit versions

--- generator/src/js/helpers/minver.js
+++ generator/src/js/helpers/minver.js
@@ -1,6 +1,27 @@
 import normalizever from './normalizever.js';
-import { gte } from 'semver';
+import { diff, gte, parse } from 'semver';
 
 export default (minimumver, curver) => {
-  return gte(normalizever(curver), normalizever(minimumver));
+  // normalize the versions
+  minimumver = parse(normalizever(minimumver));
+  curver = parse(normalizever(curver));
+
+  // we do "prerelease" comparisons a bit differently than semver does
+  if (diff(minimumver, curver) === 'prerelease') {
+    // if the minimum version doesn't have a prerelease tag, we simply return true,
+    // e.g. 0.9.8h is always greater than 0.9.8
+    if (minimumver.prerelease.length === 0) {
+      return true;
+    }
+
+    // if it's a number, we compare numbers
+    if (parseInt(minimumver.prerelease[0]) && parseInt(curver.prerelease[0])) {
+      return parseInt(curver.prerelease[0]) > parseInt(minimumver.prerelease[0]);
+    }
+
+    // otherwise, we just compare strings
+    return curver.prerelease[0] > minimumver.prerelease[0];
+  }
+
+  return gte(curver, minimumver);
 };
--- generator/src/js/helpers/normalizever.js
+++ generator/src/js/helpers/normalizever.js
@@ -2,12 +2,17 @@ import { coerce } from 'semver';
 
 export default (version) => {
   const letterRE = /([abcdefghijlmnopqrstuvwxyz]+$)/;
+  let prerelease = '';
 
   // convert to string, just in case
   version = String(version);
 
-  // get the prerelease tag
-  const prerelease = version.match(letterRE) !== null ? `-${version.match(letterRE)[0]}` : '';
+  // if we have a four digit version, e.g. 1.1.1.1
+  if (version.split('.').length > 3) {
+    prerelease = `-${version.split('.').splice(3).join('.')}`;
+  } else {
+    prerelease = version.match(letterRE) !== null ? `-${version.match(letterRE)[0]}` : '';
+  }
 
   // return version;
   return `${coerce(version).version}${prerelease}`;
