bc6eba0ac78dd5cc58c280579ca7af6d5627852a
April King <april@mozilla.com>
2019-05-08 16:01:21 -0700

    Add analyzer ability

--- generator/config/webpack.config.js
+++ generator/config/webpack.config.js
@@ -1,6 +1,7 @@
 const constants = require('../src/js/constants.js');
 const profiles = require('../src/js/profiles.js');
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const BrowserSyncWebpackPlugin = require('browser-sync-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -10,6 +11,54 @@ const path = require('path');
 const webpack = require('webpack');
 const production = process.env.NODE_ENV === 'production';
 
+
+// the many plugins used
+const plugins = [
+  new CleanWebpackPlugin(
+    ['build/*/*/*', 'build/*/*', 'build/*', 'docs/*/*/*', 'docs/*/*', 'docs/*'],
+    {
+      root: path.resolve(__dirname, '..'),
+      verbose: true
+    }
+  ),
+  new webpack.ProvidePlugin({
+    jQuery: 'jquery',
+    $: 'jquery',
+  }),
+  new HtmlWebpackPlugin({
+    constants,
+    profiles,
+    title: 'Mozilla SSL Configuration Generator',
+    template: 'src/templates/index.ejs'
+  }),
+  new CopyWebpackPlugin([
+    {
+      from: 'src/images',
+      to: 'images/',
+      flatten: false
+    }
+  ]),
+  new MiniCssExtractPlugin({
+    filename: '[hash].index.css',
+  })
+];
+
+// either we analyze or watch
+if (process.env.NODE_ENV === 'analyze') {
+  plugins.push(
+    new BundleAnalyzerPlugin({})
+  )
+} else {
+  plugins.push(
+    new BrowserSyncWebpackPlugin({
+      host: 'localhost',
+      port: 5500,
+      server: {
+        baseDir: 'build'
+      }
+    })
+  )
+}
 
 module.exports = {
   output: {
@@ -104,40 +153,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new CleanWebpackPlugin(
-      ['build/*/*/*', 'build/*/*', 'build/*', 'docs/*/*/*', 'docs/*/*', 'docs/*'],
-      {
-        root: path.resolve(__dirname, '..'),
-        verbose: true
-      }
-    ),
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery',
-    }),
-    new HtmlWebpackPlugin({
-      constants,
-      profiles,
-      title: 'Mozilla SSL Configuration Generator',
-      template: 'src/templates/index.ejs'
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'src/images',
-        to: 'images/',
-        flatten: false
-      }
-    ]),
-    new MiniCssExtractPlugin({
-      filename: '[hash].index.css',
-    }),
-    new BrowserSyncWebpackPlugin({
-      host: 'localhost',
-      port: 5500,
-      server: {
-        baseDir: 'build'
-      }
-    })
-  ]
+  plugins: plugins,
 };
